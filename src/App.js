2eed07aae769ad099a21e23865b216ad8c217568
Simon Hoyos <shmesa23@gmail.com>
2020-07-16 15:18:19 -0500

    Solution

--- src/App.js
+++ src/App.js
@@ -22,12 +22,40 @@ class App extends Component {
           { name: 'task 8', stage: 2 },
           { name: 'task 9', stage: 3 },
       ],
+      editingTask: null,
     };
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
   }
 
-  render() {
+  editTask = (name) => (e) => {
     const { tasks } = this.state;
+    const [ editingTask ] = tasks.filter(task => task.name === name);
+
+    this.setState({ editingTask });
+  }
+
+  moveBack = (e) => {
+    const { editingTask, tasks } = this.state;
+    const newTask = { ...editingTask, stage: editingTask.stage - 1 }
+    const newTasks = tasks.map(task => {
+      return task.name === editingTask.name ? newTask : task;
+    });
+
+    this.setState({ tasks: newTasks, editingTask: newTask });
+  }
+
+  moveForward = (e) => {
+    const { editingTask, tasks } = this.state;
+    const newTask = { ...editingTask, stage: editingTask.stage + 1}
+    const newTasks = tasks.map(task => {
+      return task.name === editingTask.name ? newTask : task;
+    });
+
+    this.setState({ tasks: newTasks, editingTask: newTask });
+  }
+
+  render() {
+    const { tasks, editingTask } = this.state;
 
     let stagesTasks = [];
     for (let i = 0; i < NUM_STAGES; ++i) {
@@ -40,10 +68,16 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Controls />
+        <Controls
+          stages={NUM_STAGES}
+          editingTask={editingTask}
+          moveBack={this.moveBack}
+          moveForward={this.moveForward}
+        />
         <Board
           stagesTasks={stagesTasks}
           stagesNames={this.stagesNames}
+          editTask={this.editTask}
         />
       </div>
     );
